69523274a4fdcd42961af137744dbcffada4b67a
szy0syz <szy0syz@gmail.com>
2017-12-02 23:11:52 +0800

    update. CH16-WebWorker-渲染UI主线程单线

---
+++ CH16_HTML5Core/WebWorker/js/1_delay.js
@@ -0,0 +1,23 @@
+console.log('脚本开始执行...');
+// 质数，又称素数，指在大于1的自然数中，除了1和此整数自身外，无法被其他自然数整除的数。
+var n = 5, time = 2000;
+var stamp = new Date().getTime();
+var dtime;
+do{
+  dtime = new Date().getTime() - stamp;
+} while (dtime < time);
+
+console.log(n + ' 判断质数的结果为：' + isPrime(n));
+
+console.log('脚本执行完毕...');
+
+function isPrime(num) {
+  var result = true;
+  for (var i = 2; i < num; i++) {
+    if (num % i === 0) {
+      result = false;
+      break;
+    }
+  }
+  return result;
+}
